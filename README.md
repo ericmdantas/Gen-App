c74c8005d5b16ffefc4556c63a527f36fdae03d2
George Edwards <g.edwards@gas-sense.co.uk>
2016-03-12 10:12:48 +0000

    Removed gulp-minify-css -> gulp-clean-css

---
+++ README.md
@@ -0,0 +1,3 @@
+# genapp
+[![Build Status](https://secure.travis-ci.org/georgeedwards/genapp.png?branch=master)](https://travis-ci.org/georgeedwards/genapp)
+[![Coverage Status](https://coveralls.io/repos/georgeedwards/genapp/badge.svg?branch=master)](https://coveralls.io/r/georgeedwards/genapp/?branch=master)
---
+++ server/api/todo/model/todo-model.js
@@ -0,0 +1,9 @@
+/// <reference path="../../../typings/main.d.ts" />
+"use strict";
+var mongoose = require('mongoose');
+var _todoSchema = {
+    todoMessage: { type: String, required: true, trim: true },
+    createdAt: { type: Date, default: Date.now }
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = mongoose.Schema(_todoSchema);
